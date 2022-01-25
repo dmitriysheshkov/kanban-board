c31423a4352feb30bcba60acfa6f39ae65512c87
Dmitriy Sheshkov <hello@sheshkov.ru>
2022-01-25 12:24:43 +0300

    adds js-code

---
+++ script.js
@@ -0,0 +1,42 @@
+(function() {
+    const item = document.querySelector('.item');
+    const placeholders = document.querySelectorAll('.placeholder');
+
+    item.addEventListener('dragstart', dragstart)
+    item.addEventListener('dragend', dragend)
+
+    for (const placeholder of placeholders) {
+        placeholder.addEventListener('dragover', dragover);
+        placeholder.addEventListener('dragenter', dragenter);
+        placeholder.addEventListener('dragleave', dragleave);
+        placeholder.addEventListener('drop', drop);
+    }
+
+    function dragstart(event) {
+        this.classList.add('hold');  // пришлось использовать this, т.к. target - это img
+        setTimeout(() => {
+            this.classList.add('hide'), 0;
+        });
+    }
+
+    function dragend(event) {
+        this.classList.remove('hold','hide');
+    }
+
+    function dragover(event) {
+        event.preventDefault();
+    }
+
+    function dragenter(event) {
+        event.target.classList.add('hovered');
+    }
+
+    function dragleave(event) {
+        event.target.classList.remove('hovered');
+    }
+
+    function drop(event) {
+        event.target.append(item);
+        event.target.classList.remove('hovered');
+    }
+})();
